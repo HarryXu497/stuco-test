704aa3d6927fcb284dfde8bc2eb0b052f426aeef
HarryXu497 <harryxu497@gmail.com>
2023-08-15 13:02:22 -0400

    fsd

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/public.js
+++
@@ -1,6 +0,0 @@
-const PUBLIC_SPREADSHEET_ID = "13mrLbu24-eghoaGKJnFWIx4LuWvnpTci43Z5RR2V5Sw";
-const PUBLIC_BASE_URL = "http://localhost:4173";
-export {
-  PUBLIC_BASE_URL as P,
-  PUBLIC_SPREADSHEET_ID as a
-};
